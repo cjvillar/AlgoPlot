67abd1a043a93a56bbcce4d36dc22f2f5e6e4dc3
Chris Villarreal <cjvillar@users.noreply.github.com>
2023-07-20 23:37:51 -0700

    updating plot

--- plot_sort.py
+++ plot_sort.py
@@ -4,14 +4,13 @@ import argparse
 from sorting_algorithms import *
 
 
-def visualize_sorting(sort_algorithm):
-
+def visualize_sorting(sort_algorithm, size):
     np.random.seed(0)
-    data = np.random.randint(1, 100, 50)  # change 50 to desired amount of plots to sort
+    data = np.random.randint(1, 100, size)  # change 50 to desired amount of plots to sort
 
     plt.ion()  # interactive mode for live plotting
     plt.figure()
-    bars = plt.bar(range(len(data)), data, align="center", alpha=0.5, color="Black")
+    bars = plt.bar(range(len(data)), data, align="center", alpha=0.5)
     plt.title(f"{sort_algorithm.__name__}")  # Hacky way to insert sort algo name, idk I'm tired.
     plt.xlabel("Index")
     plt.ylabel("Value")
@@ -29,7 +28,7 @@ def visualize_sorting(sort_algorithm):
 
         plt.pause(0.1)  # Pause to show the updated plot
 
-    # plt.ioff() # keep plot window open
+    #plt.ioff() # keep plot window open
     plt.pause(2)  # keep plot open for 1 sec
     plt.show()
 
@@ -39,19 +38,28 @@ if __name__ == "__main__":
     parser.add_argument(
         "algorithm",
         type=str,
+        default=50,
         choices=["bubble_sort", "insertion_sort","selection_sort", "chris_sort"],  # Add algorithms here
         help="The sorting algorithm to visualize.",
     )
+    parser.add_argument(
+        "size",
+        type=int,
+        help="The size of data to sort. default 50",
+    )
 
     args = parser.parse_args()
+    
+    size = args.size
+    algorithms = {
+        "bubble_sort": bubble_sort,
+        "insertion_sort": insertion_sort,
+        "chris_sort": chris_sort,
+        "selection_sort": selection_sort,
+        # Add more algorithms here 
+    }
 
-    if args.algorithm == "bubble_sort":
-        visualize_sorting(bubble_sort)
-    elif args.algorithm == "insertion_sort":
-        visualize_sorting(insertion_sort)
-    elif args.algorithm == "chris_sort":
-        visualize_sorting(chris_sort)
-    elif args.algorithm == "selection_sort":
-        visualize_sorting(selection_sort)
-
-        # Update as needed
+    if args.algorithm in algorithms:
+        visualize_sorting(algorithms[args.algorithm], args.size)
+    else:
+        print("Invalid algorithm name. Please choose from available algorithms.")
